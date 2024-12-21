5679f230350bcc36da38332850769abc3bcff035
blakej115 <blakej115@gmail.com>
2024-12-21 14:05:21 -0500

    Got base functionality working with minimal styling

--- src/ez-dialog.ts
+++ src/ez-dialog.ts
@@ -1,8 +1,44 @@
 export default class EzDialog {
-  private elem: HTMLDialogElement;
+  elem: HTMLDialogElement;
+  name: string;
+  triggers: Element[];
+  closes: Element[];
+  contents: Element[];
 
-  constructor(elem: HTMLDialogElement) {
+  constructor(elem: HTMLDialogElement, name: string) {
     this.elem = elem;
-    console.log(this.elem);
+    this.name = name;
+
+    this.triggers = [
+      ...document.querySelectorAll(`[ez-dialog-trigger="${name}"]`),
+    ];
+    this.closes = [...document.querySelectorAll(`[ez-dialog-close="${name}"]`)];
+    this.contents = [
+      ...document.querySelectorAll(`[ez-dialog-content="${name}"]`),
+    ];
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.triggers.forEach((trigger: Element) => {
+      trigger.addEventListener("click", () => {
+        this.elem.showModal();
+      });
+    });
+
+    this.closes.forEach((close: Element) => {
+      close.addEventListener("click", () => {
+        this.elem.close();
+      });
+    });
+
+    this.elem.addEventListener("click", (e: MouseEvent) => {
+      if (
+        !(e.target as Element).closest(`[ez-dialog-content="${this.name}"]`)
+      ) {
+        this.elem.close();
+      }
+    });
   }
 }
--- src/ez-dialogs.ts
+++ src/ez-dialogs.ts
@@ -1,7 +1,11 @@
+import "./ez-dialogs.css";
 import EzDialog from "./ez-dialog";
 
 const ezDialogs = [...document.querySelectorAll("[ez-dialog]")].map(
-  (elem: Element) => new EzDialog(<HTMLDialogElement>elem)
+  (elem: Element) => {
+    const dialogElem = elem as HTMLDialogElement;
+    return new EzDialog(dialogElem, dialogElem.getAttribute("ez-dialog")!);
+  }
 );
 
 export default ezDialogs;
